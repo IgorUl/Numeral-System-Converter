a7a7efe2e8ef700ab624dc0f7b18e5c7acdf5245
tcracked <alkoboy90@gmail.com>
2020-03-01 18:59:20 +0300

    stage 4

--- converter/Main.java
+++ converter/Main.java
@@ -1,7 +1,25 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int radix = Integer.parseInt(sc.nextLine());
+        long num;
+        if (radix == 1) {
+            num = sc.nextLine().length();
+        } else {
+            num = Long.parseLong(sc.nextLine(), radix);
+        }
+        int toRadix = sc.nextInt();
+
+        if (toRadix == 1) {
+            for (int i = 0; i < num; i++) {
+                System.out.print(1);
+            }
+        } else {
+            System.out.println(Long.toString(num, toRadix));
+        }
     }
 }
